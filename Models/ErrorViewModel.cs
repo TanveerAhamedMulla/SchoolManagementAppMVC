a3e99cd6fc1d56225e9bb05b6b65bbb1291ba4e6
Tanveer Ahamed <tanveerahemedkhan@gmail.com>
2025-05-13 07:50:38 +1000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolManagementAppMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
